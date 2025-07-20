e24568ec7aabb6b23c3e92a61301dde41b83fa3a
Thaz Sobral <thazsobral@gmail.com>
2025-07-20 19:02:27 -0300

    Changes to be committed: 	modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import br.com.dio.model.Board;
+
 public class App {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private static Board board;
+
+    private final static int BOARD_LIMITE = 9;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        final var positions = Stream.of(args)
+                                        .collect(Collectors.toMap(k -> k.split(":")[0], v -> v.split(v)));
     }
 }
